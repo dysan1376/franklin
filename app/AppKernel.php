e94435ef2c3597a2855c405a81784840afcb995c
dysan1376 <dysan1376@hotmail.com>
2018-01-30 22:29:48 -0500

    pagerfanta

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Franklin\UsuariosBundle\UsuariosBundle(),
             new Franklin\PortadaBundle\PortadaBundle(),
